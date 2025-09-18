fa90ba307461e19bc816396bca15308b0d55b915
Alagu Bala Ganapathy <65239711+alagubalu21@users.noreply.github.com>
2025-09-18 20:38:31 +0530

    Add files via upload

---
+++ identity-service/src/main/java/com/banking/indentity_service/repository/UserCredentialRepository.java
@@ -0,0 +1,10 @@
+package com.banking.indentity_service.repository;
+
+import com.banking.indentity_service.entity.UserCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserCredentialRepository  extends JpaRepository<UserCredential,Integer> {
+    Optional<UserCredential> findByName(String username);
+}
